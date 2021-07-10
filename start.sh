0fc6eccd715a26b394e559a215e32a914f4c943c
TheDogenode <86478862+TheDogenode@users.noreply.github.com>
2021-07-10 20:25:27 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 if [[ -n $TOKEN_PICKLE_URL ]]; then
-	apt-get install wget
-	wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
+	wget -q $TOKEN_PICKLE_URL -O ./token.pickle
 fi
 
 if [[ -n $ACCOUNTS_ZIP_URL ]]; then
-	apt-get install unzip
-	wget -q $ACCOUNTS_ZIP_URL -O /usr/src/app/accounts.zip
-	unzip accounts.zip -d /usr/src/app/accounts
+	wget -q $ACCOUNTS_ZIP_URL -O ./accounts.zip
+	unzip accounts.zip -d ./accounts
 	rm accounts.zip
 fi
 
