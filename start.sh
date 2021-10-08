1a13542827d3fe77f4d4ae8f4a89407d4ee8d78e
Juan <72490350+jusidama18@users.noreply.github.com>
2021-10-08 13:46:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,1 @@
-if [[ -n $TOKEN_PICKLE_URL ]]; then
-	wget -q $TOKEN_PICKLE_URL -O ./token.pickle
-fi
-
-if [[ -n $ACCOUNTS_ZIP_URL ]]; then
-	wget -q $ACCOUNTS_ZIP_URL -O ./accounts.zip
-	unzip accounts.zip -d ./accounts
-	rm accounts.zip
-fi
-
 python3 -m bot
